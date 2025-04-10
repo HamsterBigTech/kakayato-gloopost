53b1ff973e0f3b06f814bf4dacec309897677f5e
boris1592 <buzin.boria@gmail.com>
2025-04-11 02:34:29 +0300

    init

---
+++ src/routes/+page.svelte
@@ -0,0 +1,5 @@
+<h1 class="text-3xl m-5 text-center">Welcome to SvelteKit</h1>
+<p class="text-center">
+	Visit <a class="text-blue-500" href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the
+	documentation
+</p>
